54eee3ca13072f0102ffcbf135c7405e88dd4bc5
Dmitry1232321 <cuznetsov.dmitri2003@yandex.ru>
2024-07-01 21:30:18 +0300

    Добавлено отображение промежуточных точек контуров

--- ContoursDefiner/ContoursDefiner/ContourDrawer.cpp
+++ ContoursDefiner/ContoursDefiner/ContourDrawer.cpp
@@ -1,6 +1,6 @@
 #include "ContourDrawer.h"
 
-void ContourDrawer::drawWithState(HDC& hDC, double scaleX, double scaleY, ContourState state)
+void ContourDrawer::drawLines(HDC& hDC, double scaleX, double scaleY, ContourState state)
 {
   std::list<Contour>& contours = dataManager.getContours();
 
@@ -25,15 +25,48 @@ void ContourDrawer::drawWithState(HDC& hDC, double scaleX, double scaleY, Contou
   }
 }
 
+void ContourDrawer::drawPoints(HDC& hDC, double scaleX, double scaleY, ContourState state)
+{
+  std::list<Contour>& contours = dataManager.getContours();
+
+  for (auto iter = contours.begin(); iter != contours.end(); ++iter)
+  {
+    if (iter->getState() != state)
+      continue;
+
+    size_t numPoints = iter->size();
+
+    if (numPoints < 1)
+      continue;
+
+    std::vector<Point>& points = iter->getPoints();
+
+    const int offset = 5;
+    int x, y;
+    
+    for (size_t j = 0; j < numPoints; j++)
+    {
+      x = toFloatDraw(points[j].x, scaleX);
+      y = toFloatDraw(points[j].y, scaleY);
+      Ellipse(hDC, x - offset, y - offset, x + offset, y + offset);
+    }
+  }
+}
+
 void ContourDrawer::draw(HDC& hDC, double scaleX, double scaleY)
 {
   HGDIOBJ oldPen = SelectObject(hDC, visiblePen);
-  drawWithState(hDC, scaleX, scaleY, VISIBLE);
+  HGDIOBJ oldBrush = SelectObject(hDC, visibleBrush);
+  drawPoints(hDC, scaleX, scaleY, VISIBLE);
+  drawLines(hDC, scaleX, scaleY, VISIBLE);
 
   SelectObject(hDC, selectedPen);
-  drawWithState(hDC, scaleX, scaleY, SELECTED);
+  SelectObject(hDC, selectedBrush);
+  drawPoints(hDC, scaleX, scaleY, SELECTED);
+  drawLines(hDC, scaleX, scaleY, SELECTED);
 
   SelectObject(hDC, oldPen);
+  SelectObject(hDC, oldBrush);
 }
 
 
--- ContoursDefiner/ContoursDefiner/ContourDrawer.h
+++ ContoursDefiner/ContoursDefiner/ContourDrawer.h
@@ -7,7 +7,11 @@ class ContourDrawer : public AbstractDrawer
   const HPEN visiblePen;
   const HPEN selectedPen;
 
-  void drawWithState(HDC& hDC, double scaleX, double scaleY, ContourState state);
+  const HBRUSH visibleBrush;
+  const HBRUSH selectedBrush;
+
+  void drawLines(HDC& hDC, double scaleX, double scaleY, ContourState state);
+  void drawPoints(HDC& hDC, double scaleX, double scaleY, ContourState state);
 
 public:
 
@@ -15,6 +19,8 @@ public:
     : AbstractDrawer(dataManager)
     , visiblePen( CreatePen(PS_SOLID, PEN_WIDTH, RGB(0, 250, 250)) )
     , selectedPen( CreatePen(PS_SOLID, PEN_WIDTH, RGB(0, 250, 0)) )
+    , visibleBrush( CreateSolidBrush(RGB(0, 250, 250)) )
+    , selectedBrush( CreateSolidBrush(RGB(0, 250, 0)) )
   {
   }
 
